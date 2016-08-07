bf46ef46c14fd07540cd52fe8396797c3ee2266b
Kevin Kwan <KevinKwan95@googlemail.com>
2016-08-07 21:53:11 +0100

    Add start of check for configs directory.

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -12,10 +12,10 @@ COLOUR_PROMPT="yellow"
 ## Helper function to symbolically link configs in this repo to home directory.
 function linkFile {
 	# Check if already linked.
-	if [ -L ~/$1 ]; then
+	if [ -L ~/.$1 ]; then
 		print $fg[$COLOUR_OKAY] " > $1 is already linked."
 	# Check if the file already exists and prompt before action.
-	elif [ -f ~/$1 ]; then
+	elif [ -f ~/.$1 ]; then
 		print -n $fg[$COLOUR_PROMPT] " > $1 already exists as a file, do you want to remove this and link? [Y/N] "
 		read -k 1 answer
 		echo -n "\n"
@@ -30,19 +30,27 @@ function linkFile {
 		esac
 	# Link file otherwise
 	else
+		# ln -sv ~/.configs/$2 ~/.$1
 		print $fg_bold[$COLOUR_CREATE] " > $1 link created."
 	fi
 }
 
-BASEDIR=$(readlink -f "$0")
-echo "$BASEDIR"
 
 ## Entry point of script
 print $fg[$COLOUR_DEFAULT] "[Symlinks] Setup."
 
-linkFile ".gitconfig"
-linkFile ".zshrc"
-linkFile ".tmux.conf"
-linkFile ".vimrc"
+# Check .configs links to the correct folder.
+DIR_CONFIGS="$(echo $(readlink -f '$0') | sed -e 's#setup/.*#configs#')"
+DIR_CURRENT_CONFIGS="$(readlink ~/.configs)"
+
+if [ DIR_CONFIGS != DIR_CURRENT_CONFIGS ]; then
+	echo "They not the same Jim."
+fi
+
+# Link files from repository.
+linkFile "gitconfig"
+linkFile "zshrc"
+linkFile "tmux.conf"
+linkFile "vimrc"
 
 print $fg[$COLOUR_DEFAULT] "[Symlinks] Complete."
