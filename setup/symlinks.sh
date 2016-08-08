10b8334030c7c092dbea4f3f07c2a52f9005bbf0
Kevin Kwan <KevinKwan95@googlemail.com>
2016-08-08 18:47:33 +0100

    Refactor .configs to .dotfiles for simpler zshrc.

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -32,12 +32,12 @@ function linkFile {
 	# Check if already linked.
 	if [ -L $HOME/$1 ]; then
 		# Check the symlink points to the correct location.
-		if [ $(readlink $HOME/$1) = ".configs/$2" ]; then
+		if [ $(readlink $HOME/$1) = ".dotfiles/configs/$2" ]; then
 			print $fg_bold[$COLOUR_OKAY] " > $1 is already linked."
 		else
 			if promptYesNo " > $1 does not point to this repository.\n   > Relink?"; then
 				rm $HOME/$1
-				ln -s .configs/$2 $HOME/$1
+				ln -s .dotfiles/configs/$2 $HOME/$1
 				print $fg_bold[$COLOUR_CREATE] "  > $1 link created."
 			else
 				print $fg_bold[$COLOUR_NO] "   > $1 skipped."
@@ -47,14 +47,14 @@ function linkFile {
 	elif [ -f $HOME/$1 ]; then
 		if promptYesNo " > $1 already exists as a file.\n   > Remove this and link?"; then
 			rm $HOME/$1
-			ln -s .configs/$2 $HOME/$1
+			ln -s .dotfiles/configs/$2 $HOME/$1
 			print $fg_bold[$COLOUR_CREATE] "   > $1 removed and link created.";
 		else
 			print $fg_bold[$COLOUR_NO] "   > $1 skipped."
 		fi
 	# Link file otherwise
 	else
-		ln -s .configs/$2 $HOME/$1
+		ln -s .dotfiles/configs/$2 $HOME/$1
 		print $fg_bold[$COLOUR_CREATE] " > $1 link created."
 	fi
 }
@@ -63,24 +63,24 @@ function linkFile {
 ## Entry point of script
 print $fg[$COLOUR_DEFAULT] "[Symlinks] Starting."
 
-# Check .configs links to the correct folder.
-if [ -L $HOME/.configs ]; then
-	DIR_CONFIGS="$(echo $(readlink -f $0) | sed -e 's#setup/.*#configs#')"
-	DIR_CURRENT_CONFIGS="$(readlink $HOME/.configs)"
+# Check .dotfiles links to this repository.
+if [ -L $HOME/.dotfiles ]; then
+	DIR_DOTFILES="$(echo $(readlink -f $0) | sed -e 's#/setup/.*##')"
+	DIR_CURRENT_DOTFILES="$(readlink $HOME/.dotfiles)"
 
-	if [ $DIR_CONFIGS != $DIR_CURRENT_CONFIGS ]; then
-		if promptYesNo " > The current .configs symlink does not seem to point to this repository.\n   > Relink?"; then
-			rm $HOME/.configs
-			ln -s $DIR_CONFIGS $HOME/.configs
-			print $fg_bold[$COLOUR_CREATE] "  > .configs directory link removed and recreated."
+	if [ $DIR_DOTFILES != $DIR_CURRENT_DOTFILES ]; then
+		if promptYesNo " > The current .dotfiles symlink does not seem to point to this repository.\n   > Relink?"; then
+			rm $HOME/.dotfiles
+			ln -s $DIR_DOTFILES $HOME/.dotfiles
+			print $fg_bold[$COLOUR_CREATE] "  > .dotfiles directory link removed and recreated."
 		fi
 	else
-		print $fg_bold[$COLOUR_OKAY] " > .configs directory is already linked."
+		print $fg_bold[$COLOUR_OKAY] " > .dotfiles directory is already linked."
 	fi
 # Link doesn't exist, so create it.
 else
-	ln -s $DIR_CONFIGS $HOME/.configs
-	print $fg_bold[$COLOUR_CREATE] " > .configs directory link created."
+	ln -s $DIR_DOTFILES $HOME/.dotfiles
+	print $fg_bold[$COLOUR_CREATE] " > .dotfiles directory link created."
 fi
 
 
